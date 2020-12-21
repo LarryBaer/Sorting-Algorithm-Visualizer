2b72ce46145a8b8a47845f45105b9b50803b552a
Larry Baer <lbaer724@gmail.com>
2020-12-21 14:31:32 -0500

    Completed bubble sort functionality

--- js/main.js
+++ js/main.js
@@ -1,49 +1,84 @@
 var mainCanvas = document.getElementById("main_canvas");
+var bubbleButton = document.getElementById("bubble_button");
 var ctx = mainCanvas.getContext("2d");
 var linesArr = [];
-var lineDistance = 5;
-
-
-
+var swapped;
+//Creates array of different line heights
 (function createLines(){
     for(let i = 0; i < 100; i++){
         linesArr.push(randIntFromInterval(10.0, 400.0));
     }
 })();
 
-//Sort by cheating
-// var a;
-// var b;
-// linesArr = linesArr.sort((a, b) => a - b);
-
+//Gets random integer to create line heights
 function randIntFromInterval(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function drawLines(){
-    for(let i = 0; i < linesArr.length; i++){
+//Draws lines on canvas
+function drawLines(arr){
+    ctx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+    var lineDistance = 5;
+    for(let i = 0; i < arr.length; i++){
         ctx.beginPath();
         ctx.lineWidth = 5;
         ctx.moveTo(lineDistance,5);
-        ctx.lineTo(lineDistance, linesArr[i]);
+        ctx.lineTo(lineDistance, arr[i]);
         ctx.strokeStyle = '#ff0000';
         ctx.stroke();
         lineDistance += 10;
+        console.log(arr[i]);
     }
-}
-//Sort
-//Bubble Sort
-for(let i = 0; i < linesArr.length; i++){
-    for(let j = 0; j < linesArr.length-i-1; j++){
-        var numA = linesArr[i];
-        var numB = linesArr[j + 1];
-        if(numA > numB){
-            var temp = linesArr[i];
-            linesArr[i] = linesArr[j];
-            linesArr[j] = temp;
-        }
-    }
-}
+};
 
-console.log(linesArr);
-drawLines();
+//--------------------Sort--------------------
+//Bubble Sort
+function bubbleSort(arr){
+    swapped = false;
+    for(let i = 0; i < arr.length - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                swapped = true;
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+    }
+ }
+
+bubbleButton.addEventListener("click", function(){
+    do{
+        bubbleSort(linesArr);
+    }while(swapped);
+
+    console.log("BUBBLE SORTED");
+    drawLines(linesArr);
+
+});
+
+
+drawLines(linesArr);
+
+
+
+
+
+
+
+
+// do{
+//     swapped = false;
+//     for(let i = 0; i < linesArr.length - 1; i++){
+//             if(linesArr[i] > linesArr[i + 1]){
+//                 swapped = true;
+//                 let temp = linesArr[i];
+//                 linesArr[i] = linesArr[i + 1];
+//                 linesArr[i + 1] = temp;
+//             }
+//     }
+// }while(swapped);
+// drawLines(linesArr);
+
+//Sort by cheating
+// var a;
+// var b;
+// linesArr = linesArr.sort((a, b) => a - b);
