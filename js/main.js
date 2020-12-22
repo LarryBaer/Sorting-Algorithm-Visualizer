852c5e0597ff87f4450ce4bca69113ad6ccf1b02
Larry Baer <lbaer724@gmail.com>
2020-12-22 14:43:44 -0500

    Added bubble sort animation

--- js/main.js
+++ js/main.js
@@ -13,80 +13,68 @@ function randIntFromInterval(min, max){
 //Creates array of different line heights
 function createLines(){
     var tempArr = [];
-    for(let i = 0; i < 100; i++){
+    for(let i = 0; i < 50; i++){
         tempArr.push(randIntFromInterval(10.0, 400.0));
     }
     linesArr = tempArr.slice(0);
 };
 
-//Draws lines on canvas
+// Draws lines on canvas
 function drawLines(arr){
     ctx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-    var lineDistance = 5;
+    var lineDistance = 10;
     for(let i = 0; i < arr.length; i++){
         ctx.beginPath();
-        ctx.lineWidth = 5;
+        ctx.lineWidth = 10;
         ctx.moveTo(lineDistance,5);
-        ctx.lineTo(lineDistance, arr[i]);
-        ctx.strokeStyle = '#ff0000';
+		ctx.lineTo(lineDistance, arr[i]);
+		ctx.strokeStyle = '#0000ff';
         ctx.stroke();
-        lineDistance += 10;
+        lineDistance += 20;
     }
 };
 
+
 //--------------------Sort--------------------
 //Bubble Sort
-function bubbleSort(arr){
-    swapped = false;
+  
+function* bubbleSort(arr){
+do{
+	swapped = false;
     for(let i = 0; i < arr.length - 1; i++){
             if(arr[i] > arr[i + 1]){
-                swapped = true;
                 let temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
-            }
-    }
+				swapped = true;
+				yield swapped;
+			}   
+	}
+	}while(swapped);
  }
 
 bubbleButton.addEventListener("click", function(){
-    do{
-        bubbleSort(linesArr);
-    }while(swapped);
+	var sort = bubbleSort(linesArr);
 
-    drawLines(linesArr);
+	function anim(){
+	  requestAnimationFrame(anim);
+	  drawLines(linesArr);
+	}
+	setInterval(function(){	sort.next(); }, 60);
+	anim();
 
 });
 
 generateLinesBtn.addEventListener("click", function(){  
     createLines();
-    drawLines(linesArr);
+     drawLines(linesArr);
 });
-
 createLines();
-drawLines(linesArr);
+ drawLines(linesArr);
 
 
-
-
-
-
-
-
-
-// do{
-//     swapped = false;
-//     for(let i = 0; i < linesArr.length - 1; i++){
-//             if(linesArr[i] > linesArr[i + 1]){
-//                 swapped = true;
-//                 let temp = linesArr[i];
-//                 linesArr[i] = linesArr[i + 1];
-//                 linesArr[i + 1] = temp;
-//             }
-//     }
-// }while(swapped);
-// drawLines(linesArr);
-
-//Sort by cheating
+//Sort manually
 // var a;
 // var b;
 // linesArr = linesArr.sort((a, b) => a - b);
+
