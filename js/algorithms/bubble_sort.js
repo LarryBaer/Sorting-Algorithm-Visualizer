69679a9cd95747246e0dd74788ba474d2b8efddd
Larry Baer <lbaer724@gmail.com>
2020-12-26 14:41:43 -0500

    Improvied responsiveness & bubble sort animation

--- js/algorithms/bubble_sort.js
+++ js/algorithms/bubble_sort.js
@@ -1,27 +1,35 @@
-function* bubbleSort(arr){
-    do{
-        swapped = false;
-        for(let i = 0; i < arr.length - 1; i++){
-                if(arr[i] > arr[i + 1]){
-                    let temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
-                    swapped = true;
-             }   
-             swappedIndex1 = i;
-             swappedIndex2 = i + 1;
-            yield;            
-        }
-        }while(swapped);
-     }
-    
-    bubbleButton.addEventListener("click", function(){
-        var sort = bubbleSort(linesArr);
-    
-        function anim(){
-          requestAnimationFrame(anim);
-          drawLines(linesArr);
-        }
-        setInterval(function(){	sort.next(); }, 50);
-        anim();    
+let color = undefined;
+
+//bubble sort algorithm
+function* bubbleSort(arr) {
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      color = "#00ff00";
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+        color = "#FF0000";
+      }
+      swappedIndex1 = i;
+      swappedIndex2 = i + 1;
+      yield;
+    }
+  } while (swapped);
+}
+
+//Starts bubble sort animation
+bubbleButton.addEventListener("click", function() {
+  var sort = bubbleSort(linesArr);
+
+  function anim() {
+    requestAnimationFrame(anim);
+    drawLines(linesArr);
+  }
+  setInterval(function() {
+    sort.next();
+  }, 500);
+  anim();
 });
--- js/main/main.js
+++ js/main/main.js
@@ -4,54 +4,53 @@ var generateLinesBtn = document.getElementById("generate_lines");
 var ctx = mainCanvas.getContext("2d");
 var linesArr = [];
 var swapped;
-let swappedIndex1 = undefined; 
-let swappedIndex2 = undefined;  
-
+let swappedIndex1 = undefined;
+let swappedIndex2 = undefined;
 var lineDistance;
+
+
 //Gets random integer to create line heights
-function randIntFromInterval(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function randIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 //Creates array of different line heights
-function createLines(){
-    var tempArr = [];
-    for(let i = 0; i < 50; i++){
-        tempArr.push(randIntFromInterval(10.0, 400.0));
-    }
-    linesArr = tempArr.slice(0);
-};
+function createLines() {
+  var tempArr = [];
+  for (let i = 0; i < 50; i++) {
+    tempArr.push(randIntFromInterval(10.0, 400.0));
+  }
+  linesArr = tempArr.slice(0);
+}
 
 // Draws lines on canvas
-function drawLines(arr){
-    ctx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-    var lineDistance = 10;
-    for(let i = 0; i < arr.length; i++){
-        ctx.beginPath();
-        ctx.lineWidth = 10;
-        ctx.moveTo(lineDistance,5);
-        ctx.lineTo(lineDistance, arr[i]);
-        if(i === swappedIndex1 || i === swappedIndex2) { 
-              ctx.strokeStyle = '#00ff00';               
-        } else {                                         
-              ctx.strokeStyle = '#0000ff';               
-        }
-        ctx.stroke();
-        lineDistance += 20;
+function drawLines(arr) {
+  ctx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+  var lineDistance = 10;
+  for (let i = 0; i < arr.length; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.moveTo(lineDistance, 0);
+    ctx.lineTo(lineDistance, arr[i]);
+    if (i === swappedIndex1 || i === swappedIndex2) {
+      ctx.strokeStyle = color;
+    } else {
+      ctx.strokeStyle = "#45b6fe";
     }
-};
+    ctx.stroke();
+    lineDistance += 20;
+  }
+}
 
-//----------SORT----------
-//the reason it is skipping over things is bc ur setting the green ones only to things that are being swapped
-
-
-generateLinesBtn.addEventListener("click", function(){  
-    createLines();
-     drawLines(linesArr);
+//Creates new lines with button click
+generateLinesBtn.addEventListener("click", function() {
+  createLines();
+  drawLines(linesArr);
 });
+
 createLines();
 drawLines(linesArr);
-
 
 //Sort manually
 // var a;
