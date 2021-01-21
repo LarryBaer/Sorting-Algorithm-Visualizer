6456cb029945f2bfde494bfbd13e1d3cc906e6c0
Larry Baer <lbaer724@gmail.com>
2021-01-20 23:03:11 -0500

    Added bubble sort animation & refactored code

--- js/algorithms/bubble_sort.js
+++ js/algorithms/bubble_sort.js
@@ -1,41 +1,32 @@
-let color = undefined;
 var bubbleButton = document.getElementById("bubble_button");
 var comparison_count_txt = document.getElementById("comparison_count_text");
 
-//Bubble sort algorithm
-function* bubbleSort(arr) {
-  var sortLength = arr.length - 1;
-
-  do {
-    swapped = false;
-    for (let i = 0; i < sortLength; i++) {
-      comparisonCount++;
-      comparison_count_txt.innerHTML = comparisonCount;
-      color = "#00ff00";
-      if (arr[i] < arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-        color = "#FF0000";
+const bubbleSort = (array, onAction) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      onAction({ type: ACTIONS.COMPARE, data: [j, j + 1] });
+      if (array[j] < array[j + 1]) {
+        swap(array, j + 1, j);
+        onAction({ type: ACTIONS.SWAP, data: [j, j + 1] });
       }
-      yield;
     }
-    sortLength--;
-  } while (swapped);
-}
-
-//When button is pressed, start bubble sort animation
-bubbleButton.addEventListener("click", function() {
-  var sort = bubbleSort(linesArr);
-  comparisonCount = 0;
-  function anim() {
-    colors = ["#45b6fe"];
-    requestAnimationFrame(anim);
-    drawLines(linesArr, colors);
+    onAction({ type: ACTIONS.SORT, data: array.length - i - 1 });
   }
-  setInterval(function() {
-     sort.next();
-  }, sortSpeed);
-  anim();
+  return array;
+};
+
+// When button is pressed, start bubble sort animation
+bubbleButton.addEventListener("click", function() {
+  var ticks = 0;
+  bubbleSort(randomArray, (action) => {
+    ticks++;
+    setTimeout(() => {
+      actionsMap[action.type](action, lines);
+      ctx.clearRect(0, 0, innerWidth, innerHeight);
+      drawAll(lines);
+      lines.forEach((m) => m.resetColor());
+    }, ticks * sortSpeed);
+  });
 });
+
+
--- js/main/main.js
+++ js/main/main.js
@@ -6,17 +6,12 @@ var comparisonCount = 0;
 var sortSpeed = 250;
 var lineAmount = 50;
 
-//Changes the animation speed when slider is moved
-function changeSortSpeed(){
-  sortSpeed = document.getElementById("change_sort_speed").value;
-}
+const RED_COLOR = "#FF0000";
+const BLUE_COLOR = "#45b6fe";
+const GREEN_COLOR = "#00ff00";
+const GOLD_COLOR = "#FFD700";
 
-//Gets random integer to create line heights
-function randIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-//Creates array of different line heights
+// Creates array of different line heights
 function createLines() {
   var tempArr = [];
   for (let i = 0; i < lineAmount; i++) {
@@ -25,43 +20,103 @@ function createLines() {
   linesArr = tempArr.slice(0);
 }
 
-//Creates new lines with button click
+createLines();
+
+// Draws single line 
+function drawLine(x, y, width, height, color = BLUE_COLOR) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.color = color;
+
+  this.draw = () => {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+
+  this.resetColor = () => this.setColor(BLUE_COLOR);
+
+  this.setColor = (color) => {
+    if (!this.isSorted()) {
+      this.color = color;
+    }
+  };
+
+  this.isSorted = () => this.color === GOLD_COLOR;
+
+  this.sorted = () => (this.color = GOLD_COLOR);
+
+  this.setValue = (v, color) => {
+    if (!this.isSorted()) {
+      this.height = v;
+      this.setColor(color);
+    }
+  };
+  this.getValue = (v) => this.height;
+}
+
+// Get the spacing and width of lines
+const randomArray = linesArr;
+var spacing = mainCanvas.width / (2 * randomArray.length + randomArray.length + 1);
+var lineWidth = spacing * 2;
+var lineDistance = 4;
+
+// Creates array of every individual line
+const lines = randomArray.map((v) => {
+   var tempLineDistance = lineDistance;
+   lineDistance += 20;
+  return new drawLine(tempLineDistance, mainCanvas.height, lineWidth, v - mainCanvas.height);
+});
+
+// Draws all lines in array
+const drawAll = () => lines.forEach((m) => m.draw());
+drawAll()
+
+// Actions that determin what to set values
+const ACTIONS = {
+  SORT: "SORT",
+  COMPARE: "COMPARE",
+  SWAP: "SWAP",
+};
+
+const actionsMap = {
+  [ACTIONS.SORT]: (action, members) => members[action.data].sorted(),
+  [ACTIONS.SWAP]: (action, members) => {
+    const [i, j] = action.data;
+    let tmp = members[i].getValue();
+    members[i].setValue(members[j].getValue(), RED_COLOR);
+    members[j].setValue(tmp, RED_COLOR);
+  },
+  [ACTIONS.COMPARE]: (action, members) => {
+    const [i, j] = action.data;
+    members[i].setColor(GREEN_COLOR);
+    members[j].setColor(GREEN_COLOR);
+  },
+};
+
+// Changes the animation speed when slider is moved
+function changeSortSpeed(){
+  sortSpeed = document.getElementById("change_sort_speed").value;
+}
+
+// Gets random integer to create line heights
+function randIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+// Creates new lines with button click
 generateLinesBtn.addEventListener("click", function() {
   createLines();
   colors = ["#45b6fe"];
   drawLines(linesArr, colors);
 });
 
-//Swaps 2 values in an array
+// Swaps 2 values in an array
 function swap(arr, leftIndex, rightIndex){
   var temp = arr[leftIndex];
   arr[leftIndex] = arr[rightIndex];
   arr[rightIndex] = temp;
 }
-
-//Slows down animations
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function drawLines(arr, colors) {
-
-  ctx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-
-  //Get the spacing and width of lines
-  var spacing = mainCanvas.width / (2 * arr.length + arr.length + 1);
-  var lineWidth = spacing * 2;
-
-  //Draws lines
-  var lineDistance = 10;
-  for (var i = 0; i < arr.length; i++) {
-    ctx.fillStyle = colors[i];
-    ctx.fillRect(lineDistance, arr[i], lineWidth, mainCanvas.height);
-    lineDistance += spacing + lineWidth;
-  }
-}
-
-
-colors = ["#45b6fe"];
-createLines();
-drawLines(linesArr, colors);
