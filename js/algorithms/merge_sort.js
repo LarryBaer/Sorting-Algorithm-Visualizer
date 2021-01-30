45e66070226170fb47afd263cb773a9477d54d45
Larry Baer <lbaer724@gmail.com>
2021-01-29 21:35:00 -0500

    Adjusted merge sort algorithm

--- js/algorithms/merge_sort.js
+++ js/algorithms/merge_sort.js
@@ -1,37 +1,58 @@
 var mergeSortButton = document.getElementById("merge_button");
 
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+function mergeSort(arr, start, end) {
+  if (end - start > 1) {
+      var mid = start + ((end - start) >> 1);
+      
+      mergeSort(arr, start, mid);
+      mergeSort(arr, mid, end);
+      merge(arr, start, mid, end);
   }
-  var mid = Math.round((arr.length / 2));
-  var left = arr.slice(0, mid);
-  var right = arr.slice(mid);
-  return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right) {
-  sorted = [];
+function merge(arr, start, mid, end) {
+  var temp = [];
+  var length = mid - start;
+
+  var i;
+  var j;
+  var x; 
+
+  for (i = 0; i < length; i++) {
+      temp[i] = arr[start + i];
+  }
+
+  i = 0;
+  j = mid;
+  x = start;
   
-  while (left && left.length > 0 && right && right.length > 0) {
-    if (left[0] <= right[0]) {
-      sorted.push(left.shift());
-    }
-    else {
-      sorted.push(right.shift());
-    }
+  while (i < length && j < end) {
+      if (temp[i] <= arr[j]) {
+          arr[x++] = temp[i++];
+      } else {
+          arr[x++] = arr[j++];
+      }
   }
-  return sorted.concat(left, right);
+
+  while (i < length) {
+      arr[x++] = temp[i++];
+  }
 }
 
-// console.log(mergeSort(linesArr));
-
+// When button is pressed, start merge sort 
 mergeSortButton.addEventListener("click", function() {
-    var sort = mergeSort(linesArr);
-    comparisonCount = 0;
-    function anim() {
-      requestAnimationFrame(anim);
-      drawLines(linesArr, colors);
-    }
-    anim();
+  
+    // var ticks = 0;
+    // mergeSort(randomArray, action => {
+    //   ticks++;
+    //   setTimeout(() => {
+    //     actionsMap[action.type](action, lines);
+    //     ctx.clearRect(0, 0, innerWidth, innerHeight);
+    //     drawAll(lines);
+    //     lines.forEach((m) => m.resetColor());
+    //   }, ticks * sortSpeed);
+    // });
+
+  mergeSort(linesArr, 0, linesArr.length);
+  console.log(linesArr);
   });
